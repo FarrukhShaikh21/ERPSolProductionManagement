2c3717286f003d7e90e5bcec884e55231c869384
Farrukh Shaikh <52906883+FarrukhShaikh21@users.noreply.github.com>
2023-01-14 23:14:48 +0500

    ADDING BEAN

---
+++ ERPSolPMSViewController/src/erpsolpms/erpsolpmsview/erpsolpmsclass/ERPSolPMSBean.java
@@ -0,0 +1,11 @@
+package erpsolpms.erpsolpmsview.erpsolpmsclass;
+
+public class ERPSolPMSBean {
+    public ERPSolPMSBean() {
+        super();
+    }
+
+    public static void main(String[] args) {
+        ERPSolPMSBean eRPSolPMSBean = new ERPSolPMSBean();
+    }
+}
